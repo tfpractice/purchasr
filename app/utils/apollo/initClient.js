8f490bf9ffaf1b8264a75d687fc711360a816a55
theory friction practice <jackson.brebnor@gmail.com>
2017-04-18 15:11:07 -0400

    apollo url

--- app/utils/apollo/initClient.js
+++ app/utils/apollo/initClient.js
@@ -2,16 +2,16 @@ import { ApolloClient, createNetworkInterface, } from 'react-apollo';
 
 let apolloClient = null;
 
-function _initClient (headers, initialState) {
+function _initClient(headers, initialState) {
   return new ApolloClient({
     initialState,
     ssrMode: !process.browser,
     dataIdFromObject: result => result.id || null,
     networkInterface: createNetworkInterface({
-      uri: 'https://api.graph.cool/simple/v1/cixmkt2ul01q00122mksg82pn',
+      uri: 'https://us-west-2.api.scaphold.io/graphql/tfpTest',
       opts: {
         credentials: 'same-origin',
-
+        
         // Pass headers here if your graphql server requires them
       },
     }),
@@ -22,8 +22,10 @@ export const initClient = (headers, initialState = {}) => {
   if (!process.browser) {
     return _initClient(headers, initialState);
   }
+  
   if (!apolloClient) {
     apolloClient = _initClient(headers, initialState);
   }
+  
   return apolloClient;
 };
