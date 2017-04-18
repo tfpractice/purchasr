11058007c84f82be475d2f80cf85e12fbf9cafd5
theory friction practice <jackson.brebnor@gmail.com>
2017-04-18 15:09:59 -0400

    reducer store and appollo utils

---
+++ app/utils/apollo/middleware.js
@@ -0,0 +1,10 @@
+import { applyMiddleware, compose, } from 'redux';
+
+export default function createMiddleware (clientMiddleware) {
+  const middleware = applyMiddleware(clientMiddleware);
+
+  if (process.browser && window.devToolsExtension) {
+    return compose(middleware, window.devToolsExtension());
+  }
+  return middleware;
+}
