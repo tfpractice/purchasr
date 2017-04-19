8c01f9c4415fe1fd3ad837aacfebba5c7722848f
theory friction practice <jackson.brebnor@gmail.com>
2017-04-19 16:04:36 -0400

    pages

--- app/components/index.js
+++ app/components/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { ProductList, } from './products';
 export default ({ children, }) => (
   <main>
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,6 @@
+import React from 'react';
 import App from '../app/components';
 
-// import Header from '../components/Header'
-// import Submit from '../components/Submit'
-// import PostList from '../components/PostList'
 import withData from '../app/utils/apollo/withData';
 
-export default withData((props) => {
-  console.log('props', props);
-  return (<App />);
-});
+export default withData(props => (<App />));
