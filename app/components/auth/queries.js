fffa183a12da14e23ed5c22cb65ae3f9d74db4aa
theory friction practice <jackson.brebnor@gmail.com>
2017-04-24 19:22:01 -0400

    updates pkgs

--- app/components/auth/queries.js
+++ app/components/auth/queries.js
@@ -1,80 +1,76 @@
 import gql from 'graphql-tag';
+import { spread, } from 'fenugreek-collections';
 import { compose, graphql, withApollo, } from 'react-apollo';
 
 export const GetUsers = gql`
 query GetUsers($where:UserWhereArgs $first:Int) {
-  viewer {
-    allUsers(where:$where first:$first) {
-      edges {
-        node {
-          id
-          username
-        }
-      }
-    }
-  }
+   viewer {
+     allUsers(where:$where first:$first) {
+       edges {
+         node {
+           id
+           username
+         }
+       }
+     }
+   }
 }
 `;
 
 export const CreateUser = gql`
-  mutation CreateUserMutation($input: CreateUserInput!) {
-    createUser(input: $input) {
-      changedUser {
-        username
-      }
-    }
-  }
-  `;
+   mutation CreateUserMutation($input: CreateUserInput!) {
+     createUser(input: $input) {
+       changedUser {
+         username
+       }
+     }
+   }
+   `;
 
 export const LoginUser = gql`
-    mutation LoginUserMutation($input: LoginUserInput!) {
-      loginUser(input: $input) {
-        token
-        user {
-          username
-        }
-      }
-    }
-    `;
-    
+     mutation LoginUserMutation($input: LoginUserInput!) {
+       loginUser(input: $input) {
+         token
+         user {
+           username
+         }
+       }
+     }
+     `;
+     
 const getEdges = ({ data: { viewer: { allUsers: { edges, }, }, }, },
-) => (edges);
+) => spread(edges);
 
-export const executeFind = client => ({ username, }) => {
+export const executeFind = client => ({ username, }) =>
   client.query({ query: GetUsers, variables: { where: { username: { eq: username, }, }, }, })
     .then(getEdges).catch(console.error);
-};
 
 const isEmpty = (edges = []) => edges.length === 0;
 
 const log = ({ findUser, createUser, login, }) => input =>
-Promise.resolve(findUser(input)).then(isEmpty)
+Promise.resolve(findUser(input))
   .then((edges) => {
     console.log('edges', edges, isEmpty(edges));
-    return edges ?
-    createUser({ variables: { input, }, }) : login({ variables: { input, }, });
+    return isEmpty(edges) ?
+     createUser({ variables: { input, }, }) : login({ variables: { input, }, });
   })
   .then(u => console.log('login u', u) && u)
-  .catch(e => console.error('errroorr', e));
+  .catch(console.error);
 
 export const WithFind = component => graphql(GetUsers, {
-      name: 'getUsers',
-      props: ({ ownProps: { client, }, }, ) => ({ findUser: executeFind(client), }),
+       name: 'getUsers',
+       props: ({ ownProps: { client, }, }, ) => ({ findUser: executeFind(client), }),
 })(component);
 
 export const WithCreate = component => graphql(CreateUser, {
-  name: 'createUser',
-  props: ({ createUser, ownProps: { client, }, }) => ({ createUser, findUser: executeFind(client), }),
+   name: 'createUser',
+   props: ({ createUser, ownProps: { client, }, }) => ({ createUser, findUser: executeFind(client), }),
 })(component);
 
 export const WithLogin = component => graphql(LoginUser, {
-  name: 'login',
-  props: ({ login, ownProps: { findUser, createUser, }, }) => {
-    console.log('WithLoginArgs', login);
-    
-    return ({ login2: input => login({ variables: { input, }, }), login: log({ login, findUser, createUser, }), });
-  },
+   name: 'login',
+   props: ({ login, ownProps: { findUser, createUser, }, }) => ({ login: log({ login, findUser, createUser, }), }),
 })(component);
 
 export const LoginChain = component =>
-  compose(withApollo, WithCreate, WithLogin)(component);
+   compose(withApollo, WithCreate, WithLogin)(component);
