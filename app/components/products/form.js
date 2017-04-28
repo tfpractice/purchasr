2f954e71425ffbd3d2a303e239fd9c688fb763b8
theory friction practice <jackson.brebnor@gmail.com>
2017-04-28 18:10:33 -0400

    reduxFormUtils

--- app/components/products/form.js
+++ app/components/products/form.js
@@ -6,30 +6,24 @@ import Button from 'material-ui/Button';
 import TextField from 'material-ui/TextField';
 import { FormGroup, } from 'material-ui/Form';
 import { WithCreate, WithEdit, } from './containers';
-import { ClearForm, onSubmitSuccess, } from 'utils';
-const styles = { display: 'inline-flex', };
-
-// style={styles}
-// const onSubmitSuccess = (res, dispatch, { reset, }) => reset();
+import { ClearForm, onSubmitSuccess as OSS, } from 'utils';
 
 const renderField = ({ input, meta: { error: e, }, ...rest }) => (
   <TextField type="text" inputProps={input} error={e} {...rest} />
   );
 
 const baseProduct = ({ handleSubmit, destroyProduct, }) => (
-  <FormGroup onSubmit={handleSubmit} >
+  <form onSubmit={handleSubmit} >
     <FormGroup row>
       <Field label="name" name="name" component={renderField} />
       <Field label="price" name="price" type="number" component={renderField} />
     </FormGroup>
     <Button accent type="submit">Submit Product</Button>
     {destroyProduct && <Button onClick={destroyProduct}>Destroy </Button>}
-  </FormGroup>
+  </form>
   );
 
-const ReduxProduct = reduxForm({ onSubmitSuccess, })((baseProduct));
-
-// const ReduxProduct = ClearForm(baseProduct);
+const ReduxProduct = ClearForm(baseProduct);
 
 export const CreateForm = ({ createProduct, formID, }) => (
   <ReduxProduct
--- app/modules/products/actions.js
+++ app/modules/products/actions.js
@@ -3,8 +3,10 @@ import { spread, } from 'fenugreek-collections';
 export const getProducts = ({ viewer: { allProducts: { edges, }, }, }) =>
   spread(edges).map(({ product, }) => product);
 
-export const createProduct = mutate => input =>
-  mutate({ variables: { input, }, });
+export const createProduct = mutate => (input) => {
+  console.log('create input', input);
+  return mutate({ variables: { input, }, });
+};
 
 export const editProduct = mutate => id => input =>
   mutate({ variables: { input: { id, ...input, }, }, });
