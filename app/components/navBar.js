93c63ce9a5305c5d769ff16d0957f918deb188cd
theory friction practice <jackson.brebnor@gmail.com>
2017-05-01 18:59:50 -0400

    dash

--- app/components/navBar.js
+++ app/components/navBar.js
@@ -3,17 +3,20 @@ import AppBar from 'material-ui/AppBar';
 import Toolbar from 'material-ui/Toolbar';
 import Text from 'material-ui/Typography';
 import Layout from 'material-ui/Layout';
-import { Login, } from './auth';
+import { Login, WithCurrent, } from './auth';
 
-const NavBar = () => (
-  <AppBar>
-    <Toolbar>
-      <Layout container justify={'space-between'}>
-        <Text type="headline" colorInherit>Pruchasr</Text>
-        <Login formID={'mainLogin'} />
-      </Layout>
-    </Toolbar>
-  </AppBar>
-);
+const NavBar = (props) => {
+  console.log('props', props);
+  return (
+    <AppBar>
+      <Toolbar>
+        <Layout container justify={'space-between'}>
+          <Text type="headline" colorInherit>Pruchasr</Text>
+          <Login formID={'mainLogin'} />
+        </Layout>
+      </Toolbar>
+    </AppBar>
+  );
+};
 
-export default NavBar;
+export default WithCurrent(NavBar);
