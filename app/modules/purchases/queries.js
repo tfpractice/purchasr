31bb4446e53c14d318da796df3ebf532e7e41b64
theory friction practice <jackson.brebnor@gmail.com>
2017-05-02 15:01:16 -0400

    refactors purchases

--- app/modules/purchases/queries.js
+++ app/modules/purchases/queries.js
@@ -30,3 +30,17 @@ export const UNPURCHASE_PRODUCT = gql`
   }
 }
 ${PURCHASE_INFO}`;
+
+// export const PURCHASED_PRODUCTS = gql`
+//   query GetAllProducts($where:ProductWhereArgs $orderBy:[ProductOrderByArgs]) {
+//     viewer {
+//       allProducts(where:$where orderBy:$orderBy) {
+//         edges {
+//            product:node {
+//              ...productInfo
+//           }
+//         }
+//       }
+//     }
+//   }
+// ${PRODUCT_INFO}`;
